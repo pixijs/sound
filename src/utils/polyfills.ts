8044438424f50a41fb64a6b550c066d07195d73c
Matt Karl <matt.karl@jibo.com>
2017-04-20 14:00:41 -0400

    Fix promise deprecation

--- src/utils/polyfills.ts
+++ src/utils/polyfills.ts
@@ -2,6 +2,6 @@ const root = window as any;
 
 // IE 9-11 are missing Promise
 // Need for play sounds before load
-if (typeof root.Promise === undefined) {
+if (typeof root.Promise === "undefined") {
     root.Promise = require('promise-polyfill');
 }
