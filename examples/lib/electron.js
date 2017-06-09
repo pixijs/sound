0f3cba9a4bc47e1232f912de97cd20b1f4ab6c7d
Matt Karl <matt.karl@jibo.com>
2017-06-09 11:15:18 -0400

    Don't exclude lib folder

---
+++ examples/lib/electron.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const path = require('path');
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+module.exports = function(file)
+{
+    let mainWindow = null;
+
+    app.on('window-all-closed', () => {
+        app.quit();
+    });
+    
+    app.on('ready', () => {
+        mainWindow = new BrowserWindow({
+            width: 800,
+            height: 600,
+            useContentSize: true,
+            resizable: true
+        });
+
+        mainWindow.loadURL(`file://${path.dirname(__dirname)}/${file}`);
+        mainWindow.on('closed', () => {
+            mainWindow = null;
+        });
+    });
+};
