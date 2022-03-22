d2d42693e141d239302456d4f208ab2f6f23fa98
Lee, Kyutae <gorisanson@gmail.com>
2022-03-22 20:36:38 +0900

    Update WebAudioContext.ts

--- src/webaudio/WebAudioContext.ts
+++ src/webaudio/WebAudioContext.ts
@@ -77,7 +77,7 @@ class WebAudioContext extends Filterable implements IMediaContext
         this._ctx = ctx;
         // ios11 safari's webkitOfflineAudioContext allows only 44100 Hz sample rate
         this._offlineCtx = new WebAudioContext.OfflineAudioContext(1, 2,
-            ((win.OfflineAudioContext) && (ctx.sampleRate >= 8000 && ctx.sampleRate <= 96000)) ? ctx.sampleRate : 44100);
+            (win.OfflineAudioContext && (ctx.sampleRate >= 8000 && ctx.sampleRate <= 96000)) ? ctx.sampleRate : 44100);
         this._unlocked = false;
 
         this.compressor = compressor;
